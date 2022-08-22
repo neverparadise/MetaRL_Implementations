dd20eae2093a1eb6751683fcde575148ba79688a
KukJinKim <ye20013@gmail.com>
2022-08-22 01:15:52 +0000

    2022-08-22

--- reproduce_metaworld50/driver.py
+++ reproduce_metaworld50/driver.py
@@ -1,4 +1,4 @@
-import ray
+
 from collections import namedtuple
 import metaworld
 import random
@@ -9,6 +9,7 @@ import time
 from distutils.util import strtobool
 
 import gym
+
 import numpy as np
 import torch
 import torch.nn as nn
@@ -17,28 +18,113 @@ import torch.nn.functional as F
 from torch.distributions.normal import Normal
 from torch.utils.tensorboard import SummaryWriter
 
-device0 = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-device1 = torch.device("cuda:1" if torch.cuda.is_available() else "cpu")
-device2 = torch.device("cuda:2" if torch.cuda.is_available() else "cpu")
-device3 = torch.device("cuda:3" if torch.cuda.is_available() else "cpu")
-device4 = torch.device("cuda:4" if torch.cuda.is_available() else "cpu")
-device5 = torch.device("cuda:5" if torch.cuda.is_available() else "cpu")
-device6 = torch.device("cuda:6" if torch.cuda.is_available() else "cpu")
-device7 = torch.device("cuda:7" if torch.cuda.is_available() else "cpu")
+import ray
+from ray.tune.registry import register_env
+from ray.rllib.agents.ppo import PPOTrainer, PPOConfig
+from ray.tune.logger import pretty_print
 
-
+# device0 = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+# device1 = torch.device("cuda:1" if torch.cuda.is_available() else "cpu")
+# device2 = torch.device("cuda:2" if torch.cuda.is_available() else "cpu")
+# device3 = torch.device("cuda:3" if torch.cuda.is_available() else "cpu")
+# device4 = torch.device("cuda:4" if torch.cuda.is_available() else "cpu")
+# device5 = torch.device("cuda:5" if torch.cuda.is_available() else "cpu")
+# device6 = torch.device("cuda:6" if torch.cuda.is_available() else "cpu")
+# device7 = torch.device("cuda:7" if torch.cuda.is_available() else "cpu")
 
 env_names = metaworld.ML1.ENV_NAMES
 print(env_names)
 print(len(env_names))
 env_list = []
-def env_creater(env_name, SEED):
-    random.seed(SEED)
-    env.seed(SEED)
+
+def env_creator(env_config):
+    env_name = env_config["env"]
+    SEED = env_config["seed"]
     ml1 = metaworld.ML1(env_name, seed=SEED)
     env = ml1.train_classes[env_name]()
+    env.seed(SEED)
+    random.seed(SEED)
     task = random.choice(ml1.train_tasks)
+    env.set_task(task) 
     env_list.append(env)
     return env
-    
 
+for env_name in env_names:
+    register_env(env_name, env_creator)
+
+for env_name in env_names:
+    config = PPOConfig()
+    config.training(
+            gamma=0.99,
+            lr=0.0005,
+            train_batch_size=1000,
+            model={
+                    "fcnet_hiddens": [128, 128],
+                    "fcnet_activation": "tanh",
+                    },
+            use_gae=True,
+            lambda_=0.95,
+            vf_loss_coeff=0.2, 
+            entropy_coeff=0.001,
+            num_sgd_iter=5,
+            sgd_minibatch_size=32,
+            shuffle_sequences=True,
+            )\
+        .resources(
+            num_gpus=8,
+            num_cpus_per_worker=4,
+                    )\
+        .framework(
+            framework='torch'
+        )\
+        .environment(
+            env=env_name,
+            env_config = {"env": env_name, "seed": 1}
+        )\
+        .rollouts(
+            num_rollout_workers=2,
+            num_envs_per_worker=2,
+            create_env_on_local_worker=False,
+            rollout_fragment_length=250,
+            horizon=500,
+            soft_horizon=False,
+            no_done_at_end=False,
+        )\
+        .evaluation(
+            
+        )
+
+    # trainer = config.build(env=env_name)
+    trainer = PPOTrainer(env=env_name, config=config)
+    # model = trainer.get_policy().model
+    for epoch in range(4000):
+        result = trainer.train()
+        if epoch % 200 == 0:
+            print(pretty_print(result))
+            checkpoint = trainer.save()
+            print("checkpoint saved at", checkpoint)
+
+
+
+
+
+    # model_config = {
+    #                 "fcnet_hiddens": [128, 128],
+    #                 "fcnet_activation": "tanh",
+    #                 }
+    # env_config = {"env": env_name, "seed": 1}
+    # config = {
+    #             "num_workers": 1, 
+    #             "model": model_config,
+    #             "env_config": env_config,
+    #             "framework": "torch",
+    #             "num_envs_per_worker": 1,
+    #             "horizon": 500,
+    #             "train_batch_size": 5000,
+    #             "rollout_fragment_length": 500,
+    #             "no_done_at_end": True,
+    #             "gamma": 0.99,
+    #             "lr": 0.0005,
+    #             "train_batch_size": 200,
+    #             "seed": 1,
+    #             }
